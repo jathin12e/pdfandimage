258b7f6c5c96df391ae62cea5b72b5c1d2478009
jathin12e <126076303+jathin12e@users.noreply.github.com>
2025-03-26 00:55:41 +0530

    changes

--- check.py
+++ check.py
@@ -131,4 +131,4 @@ with gr.Blocks() as demo:
 
 # Run the Gradio app with Ngrok for public URL
 if __name__ == "__main__":
-     demo.launch()
+     demo.launch(server_name="0.0.0.0", server_port=7860)
